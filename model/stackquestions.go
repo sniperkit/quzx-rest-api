77da23278bbb5168276d2649a5409219a4ad40ca
Andrey Demidov <andrey.demidov@gmail.com>
2017-02-12 16:15:54 +0300

    Add QuestionId & Tags to Question

--- model/stackquestions.go
+++ model/stackquestions.go
@@ -5,19 +5,21 @@ import "log"
 type StackQuestion struct {
 	Title string `json:"title"`
 	Link string `json:"link"`
+	QuestionId int `json:"questionid"`
+	Tags string `json:"tags"`
 }
 
 func GetStackQuestionsByClassification(classification string) ([]*StackQuestion, error) {
 
 	result := []*StackQuestion{}
-	rows, err := db.Query("SELECT Title, Link FROM StackQuestions WHERE Classification = $1", classification)
+	rows, err := db.Query("SELECT Title, Link, QuestionId, Tags FROM StackQuestions WHERE Classification = $1", classification)
 
 	if err != nil {
 		log.Println(err)
 	} else {
 		for rows.Next() {
 			q := StackQuestion{}
-			rows.Scan(&q.Title, &q.Link)
+			rows.Scan(&q.Title, &q.Link, &q.QuestionId, &q.Tags)
 			result = append(result, &q)
 		}
 	}
