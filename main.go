fa84831b5c82b601c33ccb7c96493d3b09235223
Andrey Demidov <andrey.demidov@gmail.com>
2017-02-12 16:23:51 +0300

    Listen remote connections

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/api/stack/questions/{classification}", controllers.GetStackQuestionsByClassification)
 	http.Handle("/", r)
-	http.ListenAndServe(":4000", nil)
+	http.ListenAndServe("0.0.0.0:4000", nil)
 }
