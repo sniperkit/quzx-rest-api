6f5bb0ac93f2c6ed9b02461878feab0c1e33f29a
Andrey Demidov <andrey.demidov@gmail.com>
2017-02-20 15:21:57 +0300

    get dbsettings from env variable

--- model/db.go
+++ model/db.go
@@ -4,13 +4,17 @@ import (
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"log"
+	"os"
 )
 
 var db *sqlx.DB
 
 func init() {
 
-	database, error := sqlx.Open("postgres", "user=demas password=root host=192.168.1.71 port=5432 dbname=news sslmode=disable")
+	datasourceName := "user=" + os.Getenv("DBUSER") + " password=" + os.Getenv("DBPASS") +
+		" host=" + os.Getenv("DBHOST") + " port=" + os.Getenv("DBPORT")+
+		" dbname=" + os.Getenv("DBNAME") + " sslmode=disable"
+	database, error := sqlx.Open("postgres", datasourceName)
 	if error != nil {
 		log.Fatal("Cannot find database. Received error: " + error.Error())
 	} else {
