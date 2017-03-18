c8e32ea3cbaa3b3d7c44bb00869b902eb6282a77
Andrey Demidov <andrey.demidov@gmail.com>
2017-03-18 19:24:49 +0300

    refactore: split quzx

--- pkg/quzx/quzx.go
+++ pkg/quzx/quzx.go
@@ -1,7 +1,5 @@
 package quzx
 
-import "github.com/ChimeraCoder/anaconda"
-
 type UserCredentials struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
@@ -82,44 +80,4 @@ type TaggedItem struct {
 	Link string `json:"link"`
 	Date int64 `json:"date"`
 	Source int `json:"source"`  // 1 stack
-}
-
-type FeedService interface {
-	GetAllRssFeeds() ([]*RssFeed, error)
-	GetUnreadRssFeeds(rssType int) ([]*RssFeed, error)
-	GetRssFeedById(id int) (RssFeed, error)
-	UpdateRssFeed(feed *RssFeed)
-	InsertRssFeed(feed *RssFeed)
-	GetRssItemsByFeedId(feed_id int) ([]*RssItem, error)
-	SetRssItemAsReaded(id int)
-	SetRssFeedAsReaded(feedId int)
-	UnsubscribeRssFeed(feedId int)
-}
-
-type HackerNewsService interface {
-	GetUnreadedHackerNews() ([]*HackerNews, error)
-	SetHackerNewsAsReaded(id int64)
-	SetHackerNewsAsReadedFromTime(t int64)
-	SetAllHackerNewsAsReaded()
-}
-
-type StackService interface {
-	GetStackTags() ([]*StackTag, error)
-	GetStackQuestionsByClassification(classification string) ([]*StackQuestion, error)
-	SetStackQuestionAsReaded(question_id int)
-	SetStackQuestionsAsReadedByClassification(classification string)
-	SetStackQuestionsAsReadedByClassificationFromTime(classification string, t int64)
-}
-
-type TagsService interface {
-	GetTags() ([]*Tag, error)
-	GetTaggedItemsByTagId(tagId int) ([]*TaggedItem, error)
-	InsertTaggedItemFromStockItem(questionId int, tagId int)
-	InsertTaggedItemFromRss(rssItemId int, tagId int)
-	DeleteTaggedItem(id int)
-}
-
-type TwitterService interface {
-	GetFavoritesTwits(name string) ([]anaconda.Tweet, error)
-	DestroyFavorites(id int64)
 }
---
+++ pkg/quzx/services.go
@@ -0,0 +1,44 @@
+package quzx
+
+import "github.com/ChimeraCoder/anaconda"
+
+type FeedService interface {
+	GetAllRssFeeds() ([]*RssFeed, error)
+	GetUnreadRssFeeds(rssType int) ([]*RssFeed, error)
+	GetRssFeedById(id int) (RssFeed, error)
+	UpdateRssFeed(feed *RssFeed)
+	InsertRssFeed(feed *RssFeed)
+	GetRssItemsByFeedId(feed_id int) ([]*RssItem, error)
+	SetRssItemAsReaded(id int)
+	SetRssFeedAsReaded(feedId int)
+	UnsubscribeRssFeed(feedId int)
+}
+
+type HackerNewsService interface {
+	GetUnreadedHackerNews() ([]*HackerNews, error)
+	SetHackerNewsAsReaded(id int64)
+	SetHackerNewsAsReadedFromTime(t int64)
+	SetAllHackerNewsAsReaded()
+}
+
+type StackService interface {
+	GetStackTags() ([]*StackTag, error)
+	GetStackQuestionsByClassification(classification string) ([]*StackQuestion, error)
+	SetStackQuestionAsReaded(question_id int)
+	SetStackQuestionsAsReadedByClassification(classification string)
+	SetStackQuestionsAsReadedByClassificationFromTime(classification string, t int64)
+}
+
+type TagsService interface {
+	GetTags() ([]*Tag, error)
+	GetTaggedItemsByTagId(tagId int) ([]*TaggedItem, error)
+	InsertTaggedItemFromStockItem(questionId int, tagId int)
+	InsertTaggedItemFromRss(rssItemId int, tagId int)
+	DeleteTaggedItem(id int)
+}
+
+type TwitterService interface {
+	GetFavoritesTwits(name string) ([]anaconda.Tweet, error)
+	DestroyFavorites(id int64)
+}
+
