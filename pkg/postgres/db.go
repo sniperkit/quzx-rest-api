53bf9b5d412cfd47478a9d237413d98c5e428781
Andrey Demidov <andrey.demidov@gmail.com>
2017-03-21 18:30:24 +0300

    refactore: db connection

--- pkg/postgres/db.go
+++ pkg/postgres/db.go
@@ -8,38 +8,21 @@ import (
 	"fmt"
 )
 
-type DbOptions struct {
-	user string
-	password string
-	host string
-	port string
-	dbname string
-}
-
 var db *sqlx.DB
-
-func open(ops *DbOptions) (*sqlx.DB, error) {
-
-	connString := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable",
-		ops.user, ops.password, ops.host, ops.port, ops.dbname)
-	database, error := sqlx.Open("postgres", connString)
-	if error != nil {
-		log.Fatal("Cannot find database. Received error: " + error.Error())
-		return nil, error
-	} else {
-		return database, nil
-	}
-}
 
 func init() {
 
-	var ops = DbOptions{os.Getenv("DBUSER"), os.Getenv("DBPASS"),
-		os.Getenv("DBHOST"), os.Getenv("DBPORT"), os.Getenv("DBNAME")}
+	var err error
 
-	database, error := open(&ops)
-	if error != nil {
-		panic(error)
-	} else {
-		db = database
+	connectionString := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable",
+		os.Getenv("DBUSER"),
+		os.Getenv("DBPASS"),
+		os.Getenv("DBHOST"),
+		os.Getenv("DBPORT"),
+		os.Getenv("DBNAME"))
+
+	db, err = sqlx.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
